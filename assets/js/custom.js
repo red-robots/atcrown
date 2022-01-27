20cd2a3db8b95e97ee64b21bb0d843be70515576
Lisa DeBona <annalisa.quiamco@gmail.com>
2022-01-27 03:36:26 -0500

    Modified layout on Sponsors page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,36 +7,36 @@
  */
 jQuery(document).ready(function ($) {
   $(".sponsorInfo").hover(function () {
-    var target = $(this); // $.ajax({
-    //   url : frontajax.ajaxurl,
-    //   type : 'post',
-    //   dataType : "json",
-    //   data : {
-    //     action : 'get_posttype_content',
-    //     postid : target.attr("data-id")
-    //   },
-    //   beforeSend:function(){
-    //     // $("#loaderdiv").show();
-    //     // target.attr("data-pg", nextpage);
-    //     // if(nextpage>total) {
-    //     //   $('.loadmore').remove();
-    //     // } 
-    //   },
-    //   success : function( response ) {
-    //     if(response.content) {
-    //       $("#sponsor-details").html(response.content);
-    //       $("#sponsor-details-popup").fadeIn();
-    //       $(".sponsor-wrap").addClass('fadeIn');
-    //     }
-    //   },
-    //   complete: function() {
-    //     //$("#loaderdiv").hide();
-    //     $("#close-info").on("click",function(e){
-    //       e.preventDefault();
-    //       $("#sponsor-details-popup").fadeOut();
-    //     });
-    //   }
-    // });
+    var target = $(this);
+    $.ajax({
+      url: frontajax.ajaxurl,
+      type: 'post',
+      dataType: "json",
+      data: {
+        action: 'get_posttype_content',
+        postid: target.attr("data-id")
+      },
+      beforeSend: function beforeSend() {// $("#loaderdiv").show();
+        // target.attr("data-pg", nextpage);
+        // if(nextpage>total) {
+        //   $('.loadmore').remove();
+        // } 
+      },
+      success: function success(response) {
+        if (response.content) {
+          $("#sponsor-details").html(response.content);
+          $("#sponsor-details-popup").fadeIn();
+          $(".sponsor-wrap").addClass('fadeIn');
+        }
+      },
+      complete: function complete() {
+        //$("#loaderdiv").hide();
+        $("#close-info").on("click", function (e) {
+          e.preventDefault();
+          $("#sponsor-details-popup").fadeOut();
+        });
+      }
+    });
   }, function () {
     var target = $(this);
   });
--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -9,36 +9,36 @@ jQuery(document).ready(function ($) {
   $(".sponsorInfo").hover(
     function(){
       var target = $(this);
-      // $.ajax({
-      //   url : frontajax.ajaxurl,
-      //   type : 'post',
-      //   dataType : "json",
-      //   data : {
-      //     action : 'get_posttype_content',
-      //     postid : target.attr("data-id")
-      //   },
-      //   beforeSend:function(){
-      //     // $("#loaderdiv").show();
-      //     // target.attr("data-pg", nextpage);
-      //     // if(nextpage>total) {
-      //     //   $('.loadmore').remove();
-      //     // } 
-      //   },
-      //   success : function( response ) {
-      //     if(response.content) {
-      //       $("#sponsor-details").html(response.content);
-      //       $("#sponsor-details-popup").fadeIn();
-      //       $(".sponsor-wrap").addClass('fadeIn');
-      //     }
-      //   },
-      //   complete: function() {
-      //     //$("#loaderdiv").hide();
-      //     $("#close-info").on("click",function(e){
-      //       e.preventDefault();
-      //       $("#sponsor-details-popup").fadeOut();
-      //     });
-      //   }
-      // });
+      $.ajax({
+        url : frontajax.ajaxurl,
+        type : 'post',
+        dataType : "json",
+        data : {
+          action : 'get_posttype_content',
+          postid : target.attr("data-id")
+        },
+        beforeSend:function(){
+          // $("#loaderdiv").show();
+          // target.attr("data-pg", nextpage);
+          // if(nextpage>total) {
+          //   $('.loadmore').remove();
+          // } 
+        },
+        success : function( response ) {
+          if(response.content) {
+            $("#sponsor-details").html(response.content);
+            $("#sponsor-details-popup").fadeIn();
+            $(".sponsor-wrap").addClass('fadeIn');
+          }
+        },
+        complete: function() {
+          //$("#loaderdiv").hide();
+          $("#close-info").on("click",function(e){
+            e.preventDefault();
+            $("#sponsor-details-popup").fadeOut();
+          });
+        }
+      });
     }, function() {
       var target = $(this);
     }
